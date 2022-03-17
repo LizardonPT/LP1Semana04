c693814a5897e4ea3cc7525910aebd8dab2822f4
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-17 15:29:50 +0000

    Add comments to the code

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,8 +2,13 @@
 
 namespace MyFirstMethod
 {
+    
+    /// <summary>
+    /// This program is an exercise to practice the usage of Methods
+    /// </summary>
     class Program
     {
+        //Main method calls the other methods to count
         static void Main(string[] args)
         {
             CountToN(5);
@@ -11,11 +16,13 @@ namespace MyFirstMethod
             CountTo10();
         }
 
+        //This method calls the other one to count until 10
         private static void CountTo10()
         {
             CountToN(10);
         }
 
+        //Count to the number in the argument when the method is called in Main
         private static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
